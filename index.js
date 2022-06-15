558ee07989d60055a7b25e8f750381d1d066d96c
Austin Wang <yyaustin@bu.edu>
2022-06-14 23:13:38 -0400

    add MongoDB config

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const app = express()
+const Note = require("./models/notes")
 
 const requestLogger = (req, res, next) => {
     console.log("Method:", req.method)
@@ -22,33 +24,14 @@ app.use(express.static("build"))
 app.use(express.json())
 app.use(requestLogger)
 
-let notes = [
-    {
-        id: 1,
-        content: "HTML is easy",
-        date: "2022-05-30T17:30:31.098Z",
-        important: true,
-    },
-    {
-        id: 2,
-        content: "Browser can execute only Javascript",
-        date: "2022-05-30T18:39:34.091Z",
-        important: false,
-    },
-    {
-        id: 3,
-        content: "GET and POST are the most important methods of HTTP protocol",
-        date: "2022-05-30T19:20:14.298Z",
-        important: true,
-    },
-]
-
 app.get("/", (req, res) => {
     res.send("<h1>Hello World!</h1>")
 })
 
 app.get("/api/notes", (req, res) => {
-    res.json(notes)
+    Note.find({}).then((notes) => {
+        res.json(notes)
+    })
 })
 
 app.get("/api/notes/:id", (req, res) => {
@@ -95,7 +78,7 @@ app.delete("/api/notes/:id", (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
